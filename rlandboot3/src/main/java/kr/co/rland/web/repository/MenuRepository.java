227ba9da3134cf5f1cbe9904f8c1683da471aa23
DongDong107 <jkds989@gmail.com>
2023-03-07 16:20:44 +0900

    Add 20230307 middle

--- rlandboot3/src/main/java/kr/co/rland/web/repository/MenuRepository.java
+++ rlandboot3/src/main/java/kr/co/rland/web/repository/MenuRepository.java
@@ -10,7 +10,15 @@ import kr.co.rland.web.entity.Menu;
 @Mapper
 public interface MenuRepository {
 	
-	List<Menu> findAll();	
+	List<Menu> findAll();
+	List<Menu> findAll(Integer offset, 
+						Integer size,
+						String query,
+						Integer categoryId,
+						Integer price,					
+						String orderField,
+						String orderDisr);	
+//	List<Menu> findAll(int page);
 	Menu findById(long id);
 	Menu insert(Menu menu);
 	Menu update(Menu menu);
--- rlandboot3/src/main/java/kr/co/rland/web/repository/mybatis/MbMenuRepository.java
+++ rlandboot3/src/main/java/kr/co/rland/web/repository/mybatis/MbMenuRepository.java
@@ -3,17 +3,34 @@ package kr.co.rland.web.repository.mybatis;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import kr.co.rland.web.entity.Menu;
 import kr.co.rland.web.repository.MenuRepository;
 
+//@Repository
 public class MbMenuRepository implements MenuRepository {
-
+	
+	// mybatis 콩자루에서 꺼내와 주는 친구	
+	@Autowired
+	private SqlSession session;
+	
 	@Override
 	public List<Menu> findAll() {
 		// TODO Auto-generated method stub
-		return new ArrayList<Menu>();
+		MenuRepository menuDao = session.getMapper(MenuRepository.class);
+		
+		return menuDao.findAll();
 	}
-
+	
+	@Override
+	public List<Menu> findAll(int page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	@Override
 	public Menu findById(long id) {
 		// TODO Auto-generated method stub
@@ -37,5 +54,7 @@ public class MbMenuRepository implements MenuRepository {
 		// TODO Auto-generated method stub
 		
 	}
+
+	
 
 }
