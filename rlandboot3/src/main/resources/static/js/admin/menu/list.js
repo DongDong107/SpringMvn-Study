5c76a52d6b8802977582bca4831f2409803955e3
DongDong107 <jkds989@gmail.com>
2023-03-30 18:44:38 +0900

    Add 20230330 Study : Vue, 비동기, Fetch, Promise

--- rlandboot3/src/main/resources/static/js/admin/menu/list.js
+++ rlandboot3/src/main/resources/static/js/admin/menu/list.js
@@ -9,6 +9,54 @@
 		return{
 			test:"hello"
 		};
+	},
+	methods:{
+		async categoryClickHandler(e){
+//			this.load(e.target.dataset.id, function(){
+//				console.log("도착했구나..");
+//			});
+			
+			this.load(2);
+//			console.log("click");
+//			await this.load(e.target.dataset.id);
+//			console.log(" 데이터가 도착한 후에 할 일");			
+			
+		},
+		load(cid){
+		//	fetch("/menus",(response)=>{});
+			
+			let promise = fetch("/menusdfsf11")	
+			.then(response=>{
+				console.log("나도 끼쥐~");
+				console.log(response);
+				if(!response.ok){
+					throw new Error(response);
+				}
+				return response.json();
+			})
+			.then(list=>{
+				console.log(list);	
+			})
+			.catch(error=>{
+				console.log("폭탄 받아라~~!!",error);
+			});		
+			
+			
+			/*
+			promise
+			.then((response)=>{
+				let promise = response.json();
+				promise.then(list=>{
+					console.log(list);
+				});
+			});
+			*/
+			
+//			let response = await fetch("/menus");
+//			let list = response.json();
+//			console.log(list);
+
+		}
 	}
 })	
 .mount("#main-section");
